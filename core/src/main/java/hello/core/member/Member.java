0a425728f726957430d77c133b0ae278f8e8959a
Marc416 <juni416@naver.com>
2022-08-01 23:47:37 +0900

    롬복 활용

--- core/src/main/java/hello/core/member/Member.java
+++ core/src/main/java/hello/core/member/Member.java
@@ -1,5 +1,10 @@
 package hello.core.member;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Member {
     private Long id;
     private String name;
@@ -9,6 +14,8 @@ public class Member {
         this.id = id;
         this.name = name;
         this.grade = grade;
+        // 롬복의 기능 : 세터가 없어도 쓸 수 있다.
+//        setGrade(Grade.VIP);
     }
     public Long getId() {
         return id;
@@ -18,20 +25,20 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Grade getGrade() {
-        return grade;
-    }
-
-    public void setGrade(Grade grade) {
-        this.grade = grade;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public Grade getGrade() {
+//        return grade;
+//    }
+//
+//    public void setGrade(Grade grade) {
+//        this.grade = grade;
+//    }
 
 }
